9ff41c80dca740f875e45e76a9ef74798535445c
Mateus Bombassaro Pinho <mateusbpinho@gmail.com>
2026-06-09 18:46:34 -0300

    feat: initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv() 
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=gemini_api_key)
+
+# Gere conteúdo usando o modelo rápido e atual do Gemini
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Escreva um haikai sobre programação em Python.",
+)
+
+print(response.text)
